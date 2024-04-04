82dd6d89184d11f74f78b474e8680fef8a77ad60
Orlaando <orla3288@colorado.edu>
2024-04-04 13:58:19 -0600

    remove redundent comment

--- src/main/kotlin/commands/AnnotationFeature.kt
+++ src/main/kotlin/commands/AnnotationFeature.kt
@@ -4,9 +4,6 @@ import Main
 import org.bukkit.command.CommandSender
 import org.incendo.cloud.annotations.AnnotationParser
 
-/**
- * Some feature of the annotation system that we want to showcase.
- */
 interface AnnotationFeature {
     fun registerFeature(
         main: Main,
